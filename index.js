663010bbc6d03f99ad22f76d49e2d3a567b6b64d
elroi773 <s2457@e-mirim.hs.kr>
2025-04-04 13:36:06 +0900

    하는중..

--- index.js
+++ index.js
@@ -18,3 +18,33 @@ const textmain = () => {
 
 textmain();
 setInterval(textmain, 9000);
+
+const container = document.querySelector('.scroll-container');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+container.addEventListener('mousedown', (e) => {
+    isDown = true;
+    container.classList.add('active');
+    startX = e.pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+});
+
+container.addEventListener('mouseleave', () => {
+    isDown = false;
+    container.classList.remove('active');
+});
+
+container.addEventListener('mouseup', () => {
+    isDown = false;
+    container.classList.remove('active');
+});
+
+container.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - container.offsetLeft;
+    const walk = (x - startX) * 2; // 이동 속도 조절
+    container.scrollLeft = scrollLeft - walk;
+});
