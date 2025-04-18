2d910828a4e514e55e323d13bd68fbcd5ff0695e
elroi773 <s2457@e-mirim.hs.kr>
2025-04-18 23:04:53 +0900

    .

--- Profile.js
+++ Profile.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log("Profile page loaded.");
-  });
-  
+  console.log("Profile page loaded.");
+});
+
+container.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - container.offsetLeft;
+  const walk = (x - startX) * 2; // 이동 속도 조절
+  container.scrollLeft = scrollLeft - walk;
+});
